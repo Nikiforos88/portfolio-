503df54c59faf4614c2fca62eb18227edb33f846
Nikiforos Ioannidis <nioannidis@outlook.com>
2023-12-12 22:25:26 +0200

    new stage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,18 @@ cv.addEventListener('click', function() {
     // will add this feature
     console.log('download PDF');
 });
+
+// mobile menu
+const menu = document.getElementById('nav-toggle');
+let toggle = false;
+menu.addEventListener('click', function() {
+    toggle = !toggle;
+    if (toggle) {
+        console.log('open toggle menu');
+        
+    } else {
+        console.log('close toggle menu');
+        
+    }
+
+});
